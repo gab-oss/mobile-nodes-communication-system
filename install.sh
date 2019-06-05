6f9a9e2029bafd173782974a6be7db3f03ff5cc1
JakubWes <wesolowskijakub7@gmail.com>
2019-06-05 22:03:36 +0200

    IPv6 fix

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 sudo apt install libboost-test-dev
 sudo apt install cmake
 pip install --user docker
+sudo docker network create --driver bridge --ipv6 --subnet fd15:555::/64 --subnet 172.16.238.0/24 mobile_nodes_network --attachable
 mkdir build && cd build
 cmake ..
 cd ..
